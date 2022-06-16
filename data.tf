ebc3c9c786471de7b7841c89b9d98c1478bf722e
JP-Gomez <106703616+JP-Gomezz@users.noreply.github.com>
2022-06-16 17:07:45 -0500

    Update data.tf

--- data.tf
+++ data.tf
@@ -6,3 +6,14 @@ resource "digitalocean_domain" "default" {
    name = var.domain_name
 }
 
+#connection setup
+
+connection {
+      host     = self.ipv4_address
+      type     = "ssh"
+      private_key = file("~/.ssh/id_rsa")
+      user     = "root"
+      timeout  = "60m"
+      agent = "false"
+    }
+
